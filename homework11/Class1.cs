e3a8ea2c623dbe327416982834448704e6717f88
nikhildesai6666 <desainikhil9664@gmail.com>
2022-10-12 10:47:16 +0530

    Add project files.

---
+++ homework11/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework11
+{
+    internal class Class1
+    {
+        static void Main(string[]args)
+        {
+            Student s1 = new Student( "Nikhil", 50, 65, 75);
+            s1.calculate();
+            Console.WriteLine(s1);
+
+            Student s2 = new Student("Akash", 65, 78, 45);
+            s2.calculate();
+            Console.WriteLine(s2);
+
+            Student s3 = new Student("Pranav",56,97,79);
+            s3.calculate();
+            Console.WriteLine(s3);
+            
+
+        }
+    }
+}
---
+++ homework11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace homework11
+{
+    public class Student
+    {
+        private int rollno;
+        private string name;
+        private static int count;
+        private double sub1, sub2, sub3, total, per;
+
+        public Student(string name, double sub1, double sub2, double sub3)
+        {
+            count++;
+            this.rollno = count;
+            this.name = name;
+            this.sub1 = sub1;
+            this.sub2 = sub2;
+            this.sub3 = sub3;
+        }
+        public void calculate()
+        {
+            total = sub1 + sub2 + sub3;
+            per = total / 3;
+        }
+        public override string ToString()
+        {
+            return $"Roll No {rollno} Name {name} Total {total} Percentage {per}";
+
+        }
+    }
+    
+
+}
